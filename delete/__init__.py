18d4bc2267c6991ccd08d5a817c8d9c357575169
DoseiBrayin <ardilabrayan68@gmail.com>
2024-02-28 22:48:58 -0500

    (WIP)

--- delete/__init__.py
+++ delete/__init__.py
@@ -5,7 +5,11 @@ import logging
 def main(req: func.HttpRequest, deleteItem: func.SqlRowList) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     rows = list(map(lambda r: json.loads(r.to_json()), deleteItem))
-
+    if not rows:
+        return func.HttpResponse(
+            f"No data found for id = {req.params['id']}",
+            status_code=404
+        )
     return func.HttpResponse(
         json.dumps(rows),
         status_code=200,
--- readId/__init__.py
+++ readId/__init__.py
@@ -5,7 +5,11 @@ import logging
 def main(req: func.HttpRequest, readItem: func.SqlRowList) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     rows = list(map(lambda r: json.loads(r.to_json()), readItem))
-
+    if not rows:
+        return func.HttpResponse(
+            f"No data found for id = {req.params['id']}",
+            status_code=404
+        )
     return func.HttpResponse(
         json.dumps(rows),
         status_code=200,
