2bedb55cc6ace78bd4333423e9ca61e0582fd56d
DoseiBrayin <ardilabrayan68@gmail.com>
2024-03-13 11:09:13 -0500

    (UPDATE) updating delte endpoint

--- delete/__init__.py
+++ delete/__init__.py
@@ -5,7 +5,7 @@ import logging
 def main(req: func.HttpRequest, deleteItem: func.SqlRowList) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     rows = list(map(lambda r: json.loads(r.to_json()), deleteItem))
-    if not rows:
+    if not rows or rows[0]['rowsAffected'] == 0:
         return func.HttpResponse(
             f"No data found for id = {req.params['id']}",
             status_code=404
